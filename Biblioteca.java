2fa061d403fb48f26d64e610af9a52ae1bd99229
Christopher Martinez <129039732+Chrismart04@users.noreply.github.com>
2024-10-09 19:01:44 -0600

    Update Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -9,5 +9,18 @@ public class Biblioteca{
 
         ImageIcon imagenBienvenida = new ImageIcon("imagenes/bienvenida.gif");
         ImageIcon imagenBiblioteca = new ImageIcon("imagenes/Biblioteca.jpeg");
+
+      JOptionPane.showMessageDialog(null,
+                "<html>"
+                        + "<h1 style='color:orange;'>Bienvenidos a la Biblioteca</h1>"
+                        + "<p><b><font size='5' color='black'>Miembros del grupo:</font></b></p>"
+                        + "<ul style='margin-left:15px;'>"
+                        + "<li><i><font size='4' color='gray'>Name</font></i></li>"
+                        + "<li><i><font size='4' color='gray'>Alexis Name</font></i></li>"
+                        + "</ul>"
+                        + "</html>",
+                "Bienvenidos a la biblioteca",
+                JOptionPane.INFORMATION_MESSAGE,
+                imagenBienvenida);
 }
 }
