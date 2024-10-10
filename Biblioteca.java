28581e577271e0099302ca87f91cc81bb97d0e1d
Christopher Martinez <129039732+Chrismart04@users.noreply.github.com>
2024-10-09 18:55:48 -0600

    update: add the strings to make the buttons

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,5 +1,13 @@
 public class Biblioteca{
-  public static void (args[]){
-  
+  public static void main (String args[]){
+       String[] librosHistoria = {"Historia minima de México", "Historia secreta de los mongoles", "Roma: una historia cultural"};
+        
+        String[] librosTerror = {"Terror en la biblioteca", "El resplandor"};
+       
+        String[] librosProgramacion = {"Aprender a programar en C", "Curso de programación Java",};
+        String[] categories = {"Terror", "Historia", "Programación", "Salir"};
+
+        ImageIcon imagenBienvenida = new ImageIcon("imagenes/bienvenida.gif");
+        ImageIcon imagenBiblioteca = new ImageIcon("imagenes/Biblioteca.jpeg");
 }
 }
